168f1a6e2561394980ff6821108eb631e19c8e94
kceron <karemiceron@gmail.com>
2020-06-25 14:22:36 -0400

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,21 @@
+class Backer
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+    # helper function
+    def find_projects
+        ProjectBacker.all.select do |element|
+            element.backer == self
+        end
+    end
+    # ---------------
+    def backed_projects
+        find_projects.collect do |element|
+            element.project
+        end
+    end
+end
