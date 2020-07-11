fdf2603f15cac182c65b172180fd66544de3c93d
chrisvsanchez <chrisvsanchez@gmail.com>
2020-07-11 13:17:23 -0400

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+class Backer 
+
+    attr_accessor :name
+
+def initialize(name)
+    @name = name
+end
+
+def back_project(project)
+ProjectBacker.new(project, self)
+end
+
+def backed_projects
+    binding.pry
+ProjectBacker.all.select{|project| project.backer == self}
+end
+end
